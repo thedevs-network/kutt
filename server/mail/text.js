71062ca7003fcd4a73d82113691de08b5a2c7304
David <david@drtech.co>
2018-02-16 18:51:35 +0200

    Correct phrasing

--- server/mail/text.js
+++ server/mail/text.js
@@ -1,11 +1,11 @@
 exports.verifyMailText = `Thanks for creating an account on Kutt.it.
 
-Please verify your email address via link below.
+Please verify your email address using the link below.
 
 https://{{domain}}/{{verification}}`;
 
 exports.resetMailText = `A password reset has been requested for your account.
 
-Click in button below to reset your password. (ignore if you didn't requested this).
+Please click on the button below to reset your password. There's no need to take any action if you didn't request this.
 
 https://{{domain}}/{{resetpassword}}`;
