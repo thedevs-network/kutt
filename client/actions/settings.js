81a48f0224b51b1d2d26b9ebf22432502783eba4
Oleh T <olezhek_85@list.ru>
2018-06-16 20:08:13 +0300

    Splitted actions logic

---
+++ client/actions/settings.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import cookie from 'js-cookie';
+import {
+  DELETE_DOMAIN,
+  DOMAIN_ERROR,
+  DOMAIN_LOADING,
+  API_LOADING,
+  SET_DOMAIN,
+  SET_APIKEY,
+  SHOW_DOMAIN_INPUT,
+} from './actionTypes';
+
+const deleteDomain = () => ({ type: DELETE_DOMAIN });
+const setDomainError = payload => ({ type: DOMAIN_ERROR, payload });
+const showDomainLoading = () => ({ type: DOMAIN_LOADING });
+const showApiLoading = () => ({ type: API_LOADING });
+
+export const setDomain = payload => ({ type: SET_DOMAIN, payload });
+export const setApiKey = payload => ({ type: SET_APIKEY, payload });
+export const showDomainInput = () => ({ type: SHOW_DOMAIN_INPUT });
+
+export const getUserSettings = () => async dispatch => {
+  try {
+    const { data } = await axios.get('/api/auth/usersettings', {
+      headers: { Authorization: cookie.get('token') },
+    });
+    dispatch(setDomain(data.customDomain));
+    dispatch(setApiKey(data.apikey));
+  } catch (error) {
+    //
+  }
+};
+
+export const setCustomDomain = params => async dispatch => {
+  dispatch(showDomainLoading());
+  try {
+    const { data } = await axios.post('/api/url/customdomain', params, {
+      headers: { Authorization: cookie.get('token') },
+    });
+    dispatch(setDomain(data.customDomain));
+  } catch ({ response }) {
+    dispatch(setDomainError(response.data.error));
+  }
+};
+
+export const deleteCustomDomain = () => async dispatch => {
+  try {
+    await axios.delete('/api/url/customdomain', {
+      headers: { Authorization: cookie.get('token') },
+    });
+    dispatch(deleteDomain());
+  } catch ({ response }) {
+    dispatch(setDomainError(response.data.error));
+  }
+};
+
+export const generateApiKey = () => async dispatch => {
+  dispatch(showApiLoading());
+  try {
+    const { data } = await axios.post('/api/auth/generateapikey', null, {
+      headers: { Authorization: cookie.get('token') },
+    });
+    dispatch(setApiKey(data.apikey));
+  } catch (error) {
+    //
+  }
+};
