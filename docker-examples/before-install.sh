dcc2e3654e7a596eb8db5d35028413a963c64d63
Oleh T <olezhek85@users.noreply.github.com>
2018-06-26 21:50:00 +0300

    Update before-install.sh

--- docker-examples/before-install.sh
+++ docker-examples/before-install.sh
@@ -4,10 +4,10 @@ set -ev
 DOCKER_COMPOSE_VERSION="1.21.2"
 
 # Install docker-compose
-sudo rm /usr/local/bin/docker-compose
+rm /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin
+mv docker-compose /usr/local/bin
 
 # Copy config files
 cp ./docker-examples/client-config.example.js ./client/config.js
