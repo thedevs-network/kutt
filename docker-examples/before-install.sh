abe930ef284454b11a854aec76620806229505b3
Oleh T <olezhek_85@list.ru>
2018-06-26 20:58:27 +0300

    Updated docker

---
+++ docker-examples/before-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+
+DOCKER_COMPOSE_VERSION="1.21.2"
+
+# Install docker-compose
+sudo rm /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+sudo mv docker-compose /usr/local/bin
+
+# Copy config files
+cp ./docker-examples/client-config.example.js ./client/config.js
+cp ./docker-examples/server-config.example.js ./server/config.js
