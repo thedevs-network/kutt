47388e45933a6df2df9485888309248d720835d9
poeti8 <ezzati.upt@gmail.com>
2018-11-27 23:15:18 +0330

    Remove hardcoded email config for dev

--- server/mail/mail.js
+++ server/mail/mail.js
@@ -1,28 +1,15 @@
 const config = require('../config');
 const nodemailer = require('nodemailer');
 
-let mailConfig = {};
-
-if (process.env.NODE_ENV === 'production') {
-  mailConfig = {
-    host: config.MAIL_HOST,
-    port: config.MAIL_PORT,
-    secure: config.MAIL_SECURE,
-    auth: {
-      user: config.MAIL_USER,
-      pass: config.MAIL_PASSWORD,
-    },
-  };
-} else {
-  mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-      user: 'likyqc3fcjtgdnfd@ethereal.email',
-      pass: 'fjmhsR8zJQZ3BKQath',
-    },
-  };
-}
+const mailConfig = {
+  host: config.MAIL_HOST,
+  port: config.MAIL_PORT,
+  secure: config.MAIL_SECURE,
+  auth: {
+    user: config.MAIL_USER,
+    pass: config.MAIL_PASSWORD,
+  },
+};
 
 const transporter = nodemailer.createTransport(mailConfig);
 module.exports = transporter;
