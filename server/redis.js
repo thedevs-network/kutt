3410e46ec4ac39a17a5d4ab7e1a0f5448805a6f2
Shane Holland <sholland@ip-192-168-1-117.ec2.internal>
2019-01-12 16:27:30 -0500

    Allows configuring the host and port of a redis server.  Defaults to 127.0.0.1 and 6379

--- server/redis.js
+++ server/redis.js
@@ -1,7 +1,11 @@
 const { promisify } = require('util');
 const redis = require('redis');
+const config = require('./config');
 
-const client = redis.createClient();
+const client = redis.createClient(
+  config.REDIS_PORT? config.REDIS_PORT : 6379,
+  config.REDIS_HOST? config.REDIS_HOST : '127.0.0.1'
+);
 
 exports.get = promisify(client.get).bind(client);
 exports.set = promisify(client.set).bind(client);
