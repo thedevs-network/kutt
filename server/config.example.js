aacb9818202e58f415a847299dd4f15b731b0962
poeti8 <ezzati.upt@gmail.com>
2019-05-10 09:38:01 +0430

    Add a config to disable redis

--- server/config.example.js
+++ server/config.example.js
@@ -10,6 +10,7 @@ module.exports = {
   DB_PASSWORD: '',
 
   /* Redis host and port */
+  REDIS_DISABLED: false,
   REDIS_HOST: '127.0.0.1',
   REDIS_PORT: 6379,
   REDIS_PASSWORD: '',
--- server/redis.js
+++ server/redis.js
@@ -2,12 +2,18 @@ const { promisify } = require('util');
 const redis = require('redis');
 const config = require('./config');
 
-const client = redis.createClient({
-  host: config.REDIS_HOST || '127.0.0.1',
-  port: config.REDIS_PORT || 6379,
-  password: config.REDIS_PASSWORD || '',
-});
+if (config.REDIS_DISABLED === true) {
+  exports.get = () => Promise.resolve(null);
+  exports.set = () => Promise.resolve(null);
+  exports.del = () => Promise.resolve(null);
+} else {
+  const client = redis.createClient({
+    host: config.REDIS_HOST || '127.0.0.1',
+    port: config.REDIS_PORT || 6379,
+    password: config.REDIS_PASSWORD || '',
+  });
 
-exports.get = promisify(client.get).bind(client);
-exports.set = promisify(client.set).bind(client);
-exports.del = promisify(client.del).bind(client);
+  exports.get = promisify(client.get).bind(client);
+  exports.set = promisify(client.set).bind(client);
+  exports.del = promisify(client.del).bind(client);
+}
