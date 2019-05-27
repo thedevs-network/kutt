f2dd674e2a3c5f7db7c9fa819877e731a609e9ce
Pouria <poeti8@Pourias-MacBook-Pro.local>
2019-05-27 17:54:42 +0430

    Do not use password if redis password is not provided. Resolves #176

--- server/redis.js
+++ server/redis.js
@@ -10,7 +10,7 @@ if (config.REDIS_DISABLED === true) {
   const client = redis.createClient({
     host: config.REDIS_HOST || '127.0.0.1',
     port: config.REDIS_PORT || 6379,
-    password: config.REDIS_PASSWORD || '',
+    ...(config.REDIS_PASSWORD && { password: config.REDIS_PASSWORD }),
   });
 
   exports.get = promisify(client.get).bind(client);
