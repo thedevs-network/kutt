b7c23ac1cf40e805410484de60e60a6c6e24874b
Chirica Gheorghe <chiricagheorghe@gmail.com>
2019-12-13 11:54:23 +0200

    Added DB_PORT

--- server/knex.ts
+++ server/knex.ts
@@ -10,6 +10,7 @@ const db = knex({
   client: "postgres",
   connection: {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
