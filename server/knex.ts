eb305a432f159eb571562aa22deb61ced38f307c
Chirica Gheorghe <chiricagheorghe@gmail.com>
2019-12-13 09:47:07 +0200

    Added ssl env

--- server/knex.ts
+++ server/knex.ts
@@ -12,7 +12,8 @@ const db = knex({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    ssl: process.env.DB_SSL
   }
 });
 
