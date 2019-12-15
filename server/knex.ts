94094505fbdc3e231fb068ccff3a11712a82eafb
Chirica Gheorghe <botzill@users.noreply.github.com>
2019-12-15 13:05:00 +0200

    Add SSL and DB_PORT envs (#247)
    * Added ssl env
    
    * Added DB_PORT
    
    * Changed ssl option

--- server/knex.ts
+++ server/knex.ts
@@ -10,9 +10,11 @@ const db = knex({
   client: "postgres",
   connection: {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    ssl: process.env.DB_SSL === "true"
   }
 });
 
