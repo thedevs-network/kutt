5e03ed70904cd01c6291f9b1c1712f50789be884
poeti8 <ezzati.upt@gmail.com>
2020-01-30 21:58:50 +0330

    fix: api v1 routes

--- server/__v1/index.ts
+++ server/__v1/index.ts
@@ -13,7 +13,7 @@ const router = Router();
 
 /* URL shortener */
 router.post(
-  "url/submit",
+  "/url/submit",
   cors(),
   asyncHandler(auth.apikey),
   asyncHandler(auth.jwtLoose),
@@ -23,37 +23,37 @@ router.post(
   asyncHandler(link.shortener)
 );
 router.post(
-  "url/deleteurl",
+  "/url/deleteurl",
   asyncHandler(auth.apikey),
   asyncHandler(auth.jwt),
   asyncHandler(link.deleteUserLink)
 );
 router.get(
-  "url/geturls",
+  "/url/geturls",
   asyncHandler(auth.apikey),
   asyncHandler(auth.jwt),
   asyncHandler(link.getUserLinks)
 );
 router.post(
-  "url/customdomain",
+  "/url/customdomain",
   asyncHandler(auth.jwt),
   asyncHandler(link.setCustomDomain)
 );
 router.delete(
-  "url/customdomain",
+  "/url/customdomain",
   asyncHandler(auth.jwt),
   asyncHandler(link.deleteCustomDomain)
 );
 router.get(
-  "url/stats",
+  "/url/stats",
   asyncHandler(auth.apikey),
   asyncHandler(auth.jwt),
   asyncHandler(link.getLinkStats)
 );
-router.post("url/requesturl", asyncHandler(link.goToLink));
-router.post("url/report", asyncHandler(link.reportLink));
+router.post("/url/requesturl", asyncHandler(link.goToLink));
+router.post("/url/report", asyncHandler(link.reportLink));
 router.post(
-  "url/admin/ban",
+  "/url/admin/ban",
   asyncHandler(auth.apikey),
   asyncHandler(auth.jwt),
   asyncHandler(auth.admin),
