663381270b7d16221413db96e555d2f6de3836fc
poeti8 <ezzati.upt@gmail.com>
2020-07-18 12:54:32 +0430

    feat: add description migration

---
+++ server/migrations/20200718124944_description.ts
@@ -0,0 +1,10 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<any> {
+  const hasDescription = await knex.schema.hasColumn("links", "description");
+  if (!hasDescription) {
+    await knex.schema.alterTable("links", table => {
+      table.string("description");
+    });
+  }
+}
