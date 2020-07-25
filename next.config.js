89160b9bb0d31e761dd25cddf7b122d8be253cac
poeti8 <ezzati.upt@gmail.com>
2020-07-25 21:50:34 +0430

    fix: make sure local env exists

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ const { parsed: localEnv } = require("dotenv").config();
 
 module.exports = {
   publicRuntimeConfig: {
-    CONTACT_EMAIL: localEnv.CONTACT_EMAIL,
-    SITE_NAME: localEnv.SITE_NAME,
-    DEFAULT_DOMAIN: localEnv.DEFAULT_DOMAIN,
-    RECAPTCHA_SITE_KEY: localEnv.RECAPTCHA_SITE_KEY,
-    GOOGLE_ANALYTICS: localEnv.GOOGLE_ANALYTICS,
-    REPORT_EMAIL: localEnv.REPORT_EMAIL
+    CONTACT_EMAIL: localEnv && localEnv.CONTACT_EMAIL,
+    SITE_NAME: localEnv && localEnv.SITE_NAME,
+    DEFAULT_DOMAIN: localEnv && localEnv.DEFAULT_DOMAIN,
+    RECAPTCHA_SITE_KEY: localEnv && localEnv.RECAPTCHA_SITE_KEY,
+    GOOGLE_ANALYTICS: localEnv && localEnv.GOOGLE_ANALYTICS,
+    REPORT_EMAIL: localEnv && localEnv.REPORT_EMAIL
   }
 };
