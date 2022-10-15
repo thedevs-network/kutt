8f25ea98d074f886a4eb404029d676e1bf0c1bf6
Folke Ashberg <folke.ashberg@qplix.com>
2022-10-16 01:27:43 +0200

    default https

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -52,7 +52,7 @@ export const generateId = async (domain_id: number = null) => {
 
 export const addProtocol = (url: string): string => {
   const hasProtocol = /^\w+:\/\//.test(url);
-  return hasProtocol ? url : `http://${url}`;
+  return hasProtocol ? url : `https://${url}`;
 };
 
 export const generateShortLink = (id: string, domain?: string): string => {
