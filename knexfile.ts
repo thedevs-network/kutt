5f132bba6cd3290cbdd7b8b3a53c4331787aa010
rabungan <rioloid@gmail.com>
2023-01-13 09:19:34 +0800

    fix self signed certificate error when passing DB_SSL true

--- knexfile.ts
+++ knexfile.ts
@@ -9,9 +9,7 @@ module.exports = {
       user: env.DB_USER,
       port: env.DB_PORT,
       password: env.DB_PASSWORD,
-      ssl: {
-      "rejectUnauthorized": false
-      },
+      ssl: env.DB_SSL === true ? { "rejectUnauthorized": false } : false,
     },
     migrations: {
       tableName: "knex_migrations",
