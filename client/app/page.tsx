f105ba18d08455075b67ed5703bfea602d3f0bcc
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-09-28 12:51:32 -0300

    fix: Wait for loading before init

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import { DISALLOW_ANONYMOUS_LINKS } from "../consts";
@@ -13,7 +13,6 @@ import Features from "../components/Features";
 import Footer from "../components/Footer";
 import { useStoreActions, useStoreState } from "../store";
 import cookie from "js-cookie";
-import PageLoading from '../components/PageLoading';
 
 export default function Home() {
   const isAuthenticated = useStoreState(s => s.auth.isAuthenticated);
@@ -32,21 +31,6 @@ export default function Home() {
       });
     }
   }, [logout, renew]);
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setLoading(false);
-    }
-  }, [])
-
-
-  if(loading) return (
-    <AppWrapper>
-      <PageLoading />
-    </AppWrapper>
-  )
 
   if (
     !isAuthenticated &&
--- client/store/provider.tsx
+++ client/store/provider.tsx
@@ -2,9 +2,24 @@
 
 import { StoreProvider } from "easy-peasy";
 import { initializeStore } from './store';
+import { useState, useEffect } from "react";
+import PageLoading from '../components/PageLoading';
 
 export function Providers({ children }: { children: React.ReactNode }) {
     const store = initializeStore();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+          setLoading(false);
+        }
+      }, [])    
+
+    if(loading) return (
+        <StoreProvider store={store}>
+            <PageLoading />
+        </StoreProvider>
+        )
 
     return (
         <StoreProvider store={store}>
