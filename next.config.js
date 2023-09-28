115e9cb600ff0313c358f064c7c66aebb285546d
Pedro Silva <pvasilva.dev@hotmail.com>
2023-09-28 16:41:27 -0300

    fix: set credentials

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ const { parsed: localEnv } = require("dotenv").config();
 
 module.exports = {
   publicRuntimeConfig: {
-    CONTACT_EMAIL: "pedro.silva@pinely.eu",
+    CONTACT_EMAIL: "your@email.com",
     SITE_NAME: "Kutt",
-    DEFAULT_DOMAIN: "localhost.com",
+    DEFAULT_DOMAIN: "your-domain.com",
     RECAPTCHA_SITE_KEY: null,
-    REPORT_EMAIL: "pedro.silva@pinely.eu",
+    REPORT_EMAIL: "your@email.com",
     DISALLOW_ANONYMOUS_LINKS: false,
     DISALLOW_REGISTRATION: false
   }
