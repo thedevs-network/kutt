7f913a8bdb02fe00d07555d9bd8619da2c43a20c
ImOverlord <9958853+ImOverlord@users.noreply.github.com>
2023-12-15 08:33:54 +0100

    feat: added redis status to health check (#2)
    Co-authored-by: ImOverlord <thibault-alexandre.faure@swan.io>

--- server/routes/health.ts
+++ server/routes/health.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
+import client from "../redis";
 
 const router = Router();
 
-router.get("/", (_, res) => res.send("OK"));
+router.get("/", (_, res) => {
+    const redisStatus = client.status;
+    const status = redisStatus !== "connect" ? 500 : 200;
+    res
+    .status(status)
+    .json({
+        api: "OK",
+        redis: client.status,
+    });
+});
 
 export default router;
