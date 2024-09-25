efde71ae8b051db4be6d660b5ebe78c05be4bb75
Pouria Ezzati <ezzati.upt@gmail.com>
2024-09-25 15:44:18 +0330

    remove production from knex file making it available on all environments

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,19 @@
 const env = require("./server/env");
 
 module.exports = {
-  production: {
-    client: env.DB_CLIENT,
-    connection: {
-      filename: "db/" + env.DB_FILENAME,
-      host: env.DB_HOST,
-      database: env.DB_NAME,
-      user: env.DB_USER,
-      port: env.DB_PORT,
-      password: env.DB_PASSWORD,
-      ssl: env.DB_SSL,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "server/migrations",
-      disableMigrationsListValidation: true,
-    }
+  client: env.DB_CLIENT,
+  connection: {
+    filename: "db/" + env.DB_FILENAME,
+    host: env.DB_HOST,
+    database: env.DB_NAME,
+    user: env.DB_USER,
+    port: env.DB_PORT,
+    password: env.DB_PASSWORD,
+    ssl: env.DB_SSL,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "server/migrations",
+    disableMigrationsListValidation: true,
   }
 };
