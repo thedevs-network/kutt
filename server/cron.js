2b02373956ecc15cb11657eb74c6b69edfd408b3
Pouria Ezzati <ezzati.upt@gmail.com>
2024-11-23 14:44:18 +0330

    increase cron interval

--- server/cron.js
+++ server/cron.js
@@ -4,13 +4,7 @@ const query = require("./queries");
 const utils = require("./utils");
 const env = require("./env");
 
-if (env.NON_USER_COOLDOWN) {
-  cron.schedule("* */24 * * *", function() {
-    query.ip.clear().catch();
-  });
-}
-
-// check and delete links every minute
-cron.schedule("*/15 * * * * *", function() {
+// check and delete links 30 secoonds
+cron.schedule("*/30 * * * * *", function() {
   query.link.batchRemove({ expire_in: ["<", utils.dateToUTC(new Date())] }).catch();
 });
