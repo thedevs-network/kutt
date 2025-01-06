1a41cd9708b2199f31f1b34ae6068b160aebf260
Pouria Ezzati <ezzati.upt@gmail.com>
2025-01-06 17:02:24 +0330

    bypass jwt secret env validation for migration scripts

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,11 @@
+// this configuration is for migrations only
+// and since jwt secret is not required, it's set to a placehodler string to bypass env validation
+if (process.env.NODE_ENV !== "development" && process.env.NODE_ENV !== "production") {
+  if (!process.env.JWT_SECRET) {
+    process.env.JWT_SECRET = "securekey";
+  }
+}
+
 const env = require("./server/env");
 
 const isSQLite = env.DB_CLIENT === "sqlite3" || env.DB_CLIENT === "better-sqlite3";
