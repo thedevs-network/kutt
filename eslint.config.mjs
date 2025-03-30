eee1458b5396632799a7e9cedfb7c27c86b8ffb2
manelkdm <manel.keddam01@gmail.com>
2025-03-30 17:36:38 -0400

    Add lint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { ignores: ["node_modules/*", "static/*", "custom/*", "docs/*"], },
+]);
