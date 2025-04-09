bf5ae776293072b2cb04f857cfbf590df28664bd
cptfares <itsfares11@gmail.com>
2025-04-09 11:31:49 +0100

    Content Security Policy (CSP) Update with HTMX Support

--- server/server.js
+++ server/server.js
@@ -15,27 +15,18 @@ const links = require("./handlers/links.handler");
 const routes = require("./routes");
 const utils = require("./utils");
 
-
-// run the cron jobs
-// the app might be running in cluster mode (multiple instances) so run the cron job only on one cluster (the first one)
-// NODE_APP_INSTANCE variable is added by pm2 automatically, if you're using something else to cluster your app, then make sure to set this variable
 if (env.NODE_APP_INSTANCE === 0) {
   require("./cron");
 }
 
-// intialize passport authentication library
 require("./passport");
 
-// create express app
 const app = express();
 
-// this tells the express app that it's running behind a proxy server
-// and thus it should get the IP address from the proxy server
 if (env.TRUST_PROXY) {
   app.set("trust proxy", true);
 }
 
-// Configure Helmet with a CSP that allows HTMX to work
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
@@ -66,7 +57,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// serve static
 app.use("/images", express.static("custom/images"));
 app.use("/css", express.static("custom/css", { extensions: ["css"] }));
 app.use(express.static("static"));
@@ -75,8 +65,6 @@ app.use(passport.initialize());
 app.use(locals.isHTML);
 app.use(locals.config);
 
-// template engine / serve html
-
 app.set("view engine", "hbs");
 app.set("views", [
   path.join(__dirname, "../custom/views"),
@@ -84,23 +72,17 @@ app.set("views", [
 ]);
 utils.registerHandlebarsHelpers();
 
-// if is custom domain, redirect to the set homepage
 app.use(asyncHandler(links.redirectCustomDomainHomepage));
 
-// render html pages
 app.use("/", routes.render);
 
-// handle api requests
 app.use("/api/v2", routes.api);
 app.use("/api", routes.api);
 
-// finally, redirect the short link to the target
 app.get("/:id", asyncHandler(links.redirect));
 
-// 404 pages that don't exist
 app.get("*", renders.notFound);
 
-// handle errors coming from above routes
 app.use(helpers.error);
   
 app.listen(env.PORT, () => {
