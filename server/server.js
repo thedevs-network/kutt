3633c493bd508c8b99958934a14c19a57f3527bb
Rophy Tsai <rophy@users.noreply.github.com>
2025-11-24 02:40:19 +0900

    Add support for OIDC authentication (#880)
    * feat: support for OIDC authentication
    
    * feat: make oidc button style align with login button
    
    * feat: option to disable form-based login
    
    * chore: use dexidp for local oidc server
    
    * fix showing custom domain on the table when an link is edited.
    
    closes #899
    
    * only allow login if login form is not disabled or oidc is enabled
    
    * fix creating a user when logged in with oidc for the first time
    
    * use double quotation marks
    
    * show error when oidc fails
    
    * don't redirect to oidc client if is logged in already
    
    * add key icon to open id connect
    
    * remove oidc docker compose file
    
    * allow oidc redirect if oidc is enabled
    
    * remove unncesary added checks
    
    * remove empty space
    
    * move generateRandomPassword to utils
    
    * use cookie sessions only when oidc is enabled
    
    * remove oidc app url and get redirect url from current domain
    
    * update env variables list to add oidc and disable_login_form
    
    * update oidc scope default and example value
    
    ---------
    
    Co-authored-by: Pouria Ezzati <ezzati.upt@gmail.com>

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const env = require("./env");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
 const express = require("express");
+const session = require("cookie-session");
 const helmet = require("helmet");
 const path = require("node:path");
 const hbs = require("hbs");
@@ -39,6 +40,15 @@ app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// use cookie sessions only when OIDC is enabled
+// because only OIDC is using it
+if (env.OIDC_ENABLED) {
+  app.use(session({
+    keys: [env.JWT_SECRET],
+    maxAge: 1000 * 60 * 60 * 24 * 7, // expire after seven days
+  }));
+}
 
 // serve static
 app.use("/images", express.static("custom/images"));
